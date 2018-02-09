88bed10f8bef75a0ae3fd89ad253aedd1675d9d2
granitosaurus <tinarg@protonmail.com>
2018-02-09 05:16:28 +0000

    add qtile test script

---
+++ .config/qtile/test_qtile.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+HERE=$(dirname $(readlink -f $0))
+SCREEN_SIZE=${SCREEN_SIZE:-1920x1080}
+XDISPLAY=:1
+LOG_LEVEL=${LOG_LEVEL:-INFO}
+if [[ -z PYTHON ]]; then
+    PYTHON=python
+fi
+
+Xephyr +extension RANDR -screen ${SCREEN_SIZE} ${XDISPLAY} -ac &
+XEPHYR_PID=$!
+(
+  sleep 1
+  env DISPLAY=${XDISPLAY} ${PYTHON} /usr/bin/qtile -l ${LOG_LEVEL} $@ &
+  QTILE_PID=$!
+  env DISPLAY=${XDISPLAY} xterm &
+  wait $QTILE_PID
+  kill $XEPHYR_PID
+)
